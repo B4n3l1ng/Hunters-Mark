cb820f16e212ca3fa383190540eb438fdb003088
Eric Coimbra <eric.coimbra@gmail.com>
2022-10-17 11:02:25 +0100

    main character movement

--- JS/index.js
+++ JS/index.js
@@ -11,13 +11,47 @@ const player = new Image();
 player.src = "../Images/player.png"
 
 //player position and size
-const playerStartX = 0;
-const playerStartY = canvas.height - 100;
+let playerX = 0;
+let playerY = (canvas.height/2) - 100 +50;
+
+//characters width and height
+const charWidth = 100;
+const charHeight = 100;
+
+//character movement
+let movingUp = false;
+let movingDown = false;
+
+//game controls
+let isGameOver = false;
+let gameId = 0;
 
 
 const animate = () => {
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
-    ctx.drawImage(player, 0, 350-100, 100, 100)
+    ctx.drawImage(player, playerX, playerY, charWidth, charHeight)
+    // moving main character
+    if (movingUp === true && playerX >= 0) {
+        playerY -= 3;
+    } else if (movingDown === true && playerX <= 0 + charHeight) {
+        playerY += 3
+    }
+
+
+
+
+
+
+
+
+
+
+    if (isGameOver) {
+        cancelAnimationFrame(gameId)
+      } else {
+        // Ask for a new frame
+        gameId = requestAnimationFrame(animate)
+      }
 }
 
 
@@ -26,13 +60,25 @@ const animate = () => {
 
 window.onload = () => {
     document.getElementById('startBtn').onclick =() => {
-        console.log('starting')
         startGame()
     }
     function startGame() {
         start.style.display = 'none'
         gameScreen.style.display ='block'
         animate();
+        
+        
+        document.addEventListener('keydown', event => {
+            if (event.code === 'ArrowUp') {
+              movingUp = true
+            } else if (event.code === 'ArrowDown') {
+              movingDown = true
+            }
+          })
+          document.addEventListener('keyup', () => {
+            movingUp = false
+            movingDown = false
+          })
     }
 }
 
