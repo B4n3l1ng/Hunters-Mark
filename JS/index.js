8175dc3a23dd43443b8a5286402e580a09595ae3
Eric Coimbra <eric.coimbra@gmail.com>
2022-10-18 15:31:59 +0100

    Game mostly done, need polishing

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,13 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const start = document.querySelector("#startscreen");
+const introText = document.querySelector("#intro");
 const gameScreen = document.querySelector("#game-screen");
 gameScreen.style.display = "none";
 const overScreen = document.querySelector(".gameover");
 overScreen.style.display = "none";
+const instructions = document.querySelector("#instructions");
+instructions.style.display = "none";
 const gameOverTitle = document.querySelector("#gameOverTitle");
 const scorePlace = document.querySelector("#score-num");
 const finalScore = document.querySelector("#final-score");
@@ -13,15 +16,14 @@ const easterEgg = document.querySelector("#easter");
 //sounds
 const music = new Audio("../Sounds/Background Music.mp3");
 music.volume = 0.1;
-const victory = new Audio ("/Sounds/Game Over Screen.mp3");
+const victory = new Audio("/Sounds/Game Over Screen.mp3");
 victory.volume = 0.2;
-const arrowShot = new Audio ("../Sounds/Arrow Shot.wav");
+const arrowShot = new Audio("../Sounds/Arrow Shot.wav");
 arrowShot.volume = 1;
-const dying = new Audio ("../Sounds/Dying.wav")
+const dying = new Audio("../Sounds/Dying.wav");
 dying.volume = 0.2;
-const fail = new Audio ("../Sounds/fail sound.wav")
+const fail = new Audio("../Sounds/fail sound.wav");
 fail.volume = 0.2;
-
 
 //images
 const background = new Image();
@@ -34,7 +36,8 @@ const enemyImg2 = new Image();
 enemyImg2.src = "../Images/ogre final.png";
 const enemyImg3 = new Image();
 enemyImg3.src = "../Images/goblin final.png";
-
+const arty = new Image();
+arty.src = "../Images/wolf final.png";
 
 const arrowImg = new Image();
 arrowImg.src = "../Images/arrow final.png";
@@ -46,6 +49,10 @@ let playerY = canvas.height / 2 - 100 + 50;
 //characters width and height
 const charWidth = 100;
 const charHeight = 100;
+const artyWidth = 150;
+const artyHeight = 75;
+let artyX = 0 + charHeight / 2;
+let isArtyUp = false;
 
 //character movement
 let movingUp = false;
@@ -57,68 +64,107 @@ let gameId = 0;
 let enemySpeed = 3;
 let score = 0;
 let interval = 0;
+let artyUses = 1;
 
 //enemies
 const enemies = [
-  { x: 1200, y: (Math.random()*(canvas.height-charHeight)), img: enemyImg1 },
-  { x: 1400, y: (Math.random()*(canvas.height-charHeight)), img: enemyImg2 },
-  { x: 1600, y: (Math.random()*(canvas.height-charHeight)), img: enemyImg3 },
+  { x: 1200, y: Math.random() * (canvas.height - charHeight), img: enemyImg1 },
+  { x: 1400, y: Math.random() * (canvas.height - charHeight), img: enemyImg2 },
+  { x: 1600, y: Math.random() * (canvas.height - charHeight), img: enemyImg3 },
 ];
 
 class Projectile {
-    constructor({x, y}) {
-        this.x = x;
-        this.y = y;
-        this.image = arrowImg;
-    }
+  constructor({ x, y }) {
+    this.x = x;
+    this.y = y;
+    this.image = arrowImg;
+  }
 
-    draw() {
-        ctx.drawImage(this.image, this.x, this.y, 25, 15)
-    }
-    update() {
-        this.draw()
-        this.x += enemySpeed;
-    }
+  draw() {
+    ctx.drawImage(this.image, this.x, this.y, 25, 15);
+  }
+  update() {
+    this.draw();
+    this.x += 10;
+  }
 }
 const projectiles = [];
 
 const animate = () => {
-  let scoreStr =score.toString();
+  let scoreStr = score.toString();
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
   ctx.drawImage(player, playerX, playerY, charWidth, charHeight);
-  if (score%10===0 && score !==0) {
-    if (interval === 0 ) {
-    enemySpeed+=0.5;
-    interval +=1;
-  } 
-}
+  if (score % 10 === 0 && score !== 0) {
+    if (interval === 0) {
+      enemySpeed += 1;
+      interval += 1;
+    }
+  }
   if (scoreStr[scoreStr.length - 1] === "9") {
     interval = 0;
   }
+  if (isArtyUp === true) {
+    ctx.drawImage(arty, artyX, 0, artyWidth, artyHeight);
+    ctx.drawImage(arty, artyX, 0 + artyHeight, artyWidth, artyHeight);
+    ctx.drawImage(arty, artyX, 0 + 2 * artyHeight, artyWidth, artyHeight);
+    ctx.drawImage(arty, artyX, 0 + 3 * artyHeight, artyWidth, artyHeight);
+    ctx.drawImage(arty, artyX, 0 + 4 * artyHeight, artyWidth, artyHeight);
+    ctx.drawImage(arty, artyX, 0 + 5 * artyHeight, artyWidth, artyHeight);
+    ctx.drawImage(arty, artyX, 0 + 6 * artyHeight, artyWidth, artyHeight);
+    ctx.drawImage(arty, artyX, 0 + 7 * artyHeight, artyWidth, artyHeight);
+    ctx.drawImage(arty, artyX, 0 + 8 * artyHeight, artyWidth, artyHeight);
+    artyX += 6;
+    if (artyX > 900) {
+      isArtyUp = false;
+    }
+  }
+
   //enemies moving
-  for (let i =0; i< enemies.length; i+=1) {
+  for (let i = 0; i < enemies.length; i += 1) {
     let current = enemies[i];
     ctx.drawImage(current.img, current.x, current.y, charWidth, charHeight);
     current.x -= enemySpeed;
     if (current.x < 0) {
-        current.x = 1000;
-        current.y = Math.random()*(canvas.height - charHeight);
-        score-=1;
-        scorePlace.innerHTML = score;
-    }
-    if (current.x < playerX + charWidth && current.x + charWidth > playerX && current.y < playerY + charHeight && current.y + charHeight > playerY ) {
-        isGameOver=true;
-    }
-    projectiles.forEach((projectile,index) => {
-      if (current.x < projectile.x + 25 && current.x + charWidth > projectile.x && current.y < projectile.y + 15 && current.y + charHeight > projectile.y) {
-        dying.play();
-        projectiles.splice(index, 1);
-        current.x = 1000;
-        current.y = Math.random()* (canvas.height - charHeight);
-        score +=1;
+      current.x = 900 + charWidth;
+      current.y = Math.random() * (canvas.height - charHeight);
+      if (score > 0) {
+        score -= 1;
         scorePlace.innerHTML = score;
       }
-    })
+    }
+    if (
+      current.x < playerX + charWidth &&
+      current.x + charWidth > playerX &&
+      current.y < playerY + charHeight &&
+      current.y + charHeight > playerY
+    ) {
+      isGameOver = true;
+    }
+    if (artyX < 900 - artyWidth + 1) {
+      if (current.x < artyX + artyWidth && current.x + charWidth > artyX) {
+        current.x = 900 + charWidth;
+        current.y = Math.random() * (canvas.height - charHeight);
+        score += 1;
+        scorePlace.innerHTML = score;
+      }
+    }
+
+    projectiles.forEach((projectile, index) => {
+      if (
+        current.x < projectile.x + 25 &&
+        current.x + charWidth > projectile.x &&
+        current.y < projectile.y + 15 &&
+        current.y + charHeight > projectile.y
+      ) {
+        dying.play();
+        projectiles.splice(index, 1);
+        current.x = 900 + charWidth;
+        current.y = Math.random() * (canvas.height - charHeight);
+        score += 1;
+        scorePlace.innerHTML = score;
+      }
+    });
+    //arty collisions to be made here
   }
   // moving main character
   if (movingUp === true && playerY >= 0) {
@@ -126,31 +172,32 @@ const animate = () => {
   } else if (movingDown === true && playerY <= 700 - charHeight) {
     playerY += 6;
   }
-  
   //handling projectiles
-  projectiles.forEach((projectile,index) => {
+  projectiles.forEach((projectile, index) => {
     if (projectile.x >= 900) {
-      projectiles.splice(index,1);
+      projectiles.splice(index, 1);
     } else {
       projectile.update();
     }
-  })
+  });
   if (score >= 50) {
     isGameOver = true;
   }
 
   if (isGameOver) {
-    //need to stop background music here, don't know how
     music.pause();
-    if (score>=50) {
-      gameOverTitle.innerHTML = "You saved the village!"
-      easterEgg.innerHTML = "Hawkeye has got nothing on you!"
+    if (score >= 50) {
+      gameOverTitle.innerHTML = "You saved the village!";
+      easterEgg.innerHTML = "Hawkeye has got nothing on you!";
       victory.play();
-    }else {
+    } else {
+      if (score < 0) {
+        score = 0;
+      }
       fail.play();
     }
-    overScreen.style.display="block"
-    gameScreen.style.display ="none"
+    overScreen.style.display = "block";
+    gameScreen.style.display = "none";
     cancelAnimationFrame(gameId);
     finalScore.innerHTML = score;
   } else {
@@ -160,8 +207,12 @@ const animate = () => {
 };
 
 window.onload = () => {
-    document.getElementById("startBtn").onclick = () => {
+  document.getElementById("startBtn").onclick = () => {
     startGame();
+  };
+  document.getElementById("instructionsBtn").onclick = () => {
+    instructions.style.display = "block";
+    introText.style.display = "none";
   };
   function startGame() {
     start.style.display = "none";
@@ -170,27 +221,31 @@ window.onload = () => {
     music.play();
 
     document.addEventListener("keydown", (event) => {
-        if (event.code === "ArrowUp") {
+      if (event.code === "ArrowUp") {
         movingUp = true;
       } else if (event.code === "ArrowDown") {
         movingDown = true;
-      } else if (event.code ==="Space") {
+      } else if (event.code === "Space") {
         arrowShot.play();
         projectiles.push(
-            new Projectile ({
-                x: playerX + charWidth,
-                y: playerY + charHeight/2
-            })
-            )
+          new Projectile({
+            x: playerX + charWidth,
+            y: playerY + charHeight / 2,
+          })
+        );
+      } else if (event.code === "KeyV") {
+        if (artyUses > 0) {
+          isArtyUp = true;
+        }
       }
-    })
+    });
     document.addEventListener("keyup", () => {
       movingUp = false;
       movingDown = false;
     });
   }
 
-    document.getElementById("restartBtn").onclick = () => {
-      location.reload();
-    };
-}
+  document.getElementById("restartBtn").onclick = () => {
+    location.reload();
+  };
+};
